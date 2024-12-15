a376d5a6f6554818d4b5ce1f8b414a20dec718ce
Aamir-20 <107755083+Aamir-20@users.noreply.github.com>
2024-12-15 11:23:11 +0000

    Add all final logic for the game to run

--- myscript.js
+++ myscript.js
@@ -16,7 +16,7 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-function playGame() {
+function playGame() {playGames:{
     // const humanSelection = getHumanChoice();
     // const computerSelection = getComputerChoice();
     
@@ -34,14 +34,36 @@ function playGame() {
     // we use the .forEach method to iterate through each button
     buttons.forEach((button) => {
         // and for each one we add a 'click' listener
-        button.addEventListener("click", () => {
+        button.addEventListener("click", (event) => {
+            if (humanScore >= 5 || computerScore >= 5 ){
+                return;
+            }
             const humanSelection = button.id;
             const computerSelection = getComputerChoice();
             
             console.log(`Computer's choice: ${computerSelection}`);
             console.log(`Human's choice: ${humanSelection}`);
             
-            playRound(humanSelection, computerSelection);
+            let result = playRound(humanSelection, computerSelection);
+
+            bodyRef = document.querySelector("body");  
+            const divRef = document.querySelector("div"); 
+            if (divRef !== null) {bodyRef.removeChild(divRef);}
+            
+            const div = document.createElement("div");   
+            div.textContent = `Computer's score: ${computerScore} \n Human's score: ${humanScore}`;
+            const div2 = document.createElement("div");            
+            div2.textContent = result;
+            div.appendChild(div2);
+            bodyRef.appendChild(div);
+
+            if (humanScore == 5 || computerScore == 5 ){
+                const div3 = document.createElement("div");  
+                let winner = "Human";
+                if (humanScore < computerScore){winner = "Computer";}
+                div3.textContent = winner + " is the winner!!!!";
+                div.appendChild(div3);
+            }
         });
     });
 
@@ -52,39 +74,43 @@ function playGame() {
             if (humanChoice === "rock") {
                 humanScore++;
                 console.log("You win! Rock beats Scissors");
+                return "You win! Rock beats Scissors";
             }
             else {
                 computerScore++;
                 console.log("You lose! Rock beats Scissors");
+                return "You lose! Rock beats Scissors";
             }
-            return;
         }
         else if ((humanChoice === "rock" && computerChoice === "paper") || 
             (computerChoice === "rock" && humanChoice === "paper")) {
             if (humanChoice === "paper") {
                 humanScore++;
                 console.log("You win! Paper beats Rock");
+                return "You win! Paper beats Rock";
             }
             else {
                 computerScore++;
                 console.log("You lose! Paper beats Rock");
+                return "You lose! Paper beats Rock";
             }
-            return;
         }
         else if ((humanChoice === "scissors" && computerChoice === "paper") || 
             (computerChoice === "scissors" && humanChoice === "paper")) {
             if (humanChoice === "scissors") {
                 humanScore++;
                 console.log("You win! Scissors beats Paper");
+                return "You win! Scissors beats Paper";
             }
             else {
                 computerScore++;
                 console.log("You lose! Scissors beats Paper");
+                return "You lose! Scissors beats Paper";
             }
-            return;
         }
         else {
             console.log("It's a tie!");
+            return "It's a tie!";
         }
     }
-}
+}}
